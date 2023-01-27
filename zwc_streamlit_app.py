f3900e575124ec2bedf7f6af9f53b9b91e9b910c
PHILIP NEELY <122125662+neelypm02@users.noreply.github.com>
2023-01-27 10:07:34 -0600

    Update zwc_streamlit_app.py

--- zwc_streamlit_app.py
+++ zwc_streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 import snowflake.connector
 import pandas
-streamlit.title('Zena's Amazing Athleisure Catalog')
+streamlit.title('Zena\'s Amazing Athleisure Catalog')
 # connect to snowflake
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
@@ -20,7 +20,6 @@ option = streamlit.selectbox('Pick a sweatsuit color or style:', list(color_list
 # We'll build the image caption now, since we can
 product_caption = 'Our warm, comfortable, ' + option + ' sweatsuit!'
 # use the option selected to go back and get all the info from the database
-
 
 my_cur.execute("select direct_url, price, size_list, upsell_product_desc from catalog_for_website where color_or_style = '" + option + "';")
 df2 = my_cur.fetchone()
